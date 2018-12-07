7857445faa25f454deecb838fb8761047683eb98
Sky <zk827083320@qq.com>
2018-12-07 17:57:04 +0800

    测试。

--- app/src/main/java/com/sky/lazycat/info/InfoConstants.java
+++ app/src/main/java/com/sky/lazycat/info/InfoConstants.java
@@ -15,6 +15,7 @@ public final class InfoConstants {
     public static final String KEY_CHROME_CUSTOM_TABS = "chrome_custom_tabs";
     public static final String KEY_NIGHT_MODE = "night_mode";
     public static final String KEY_TIME_OF_SAVING_ARTICLES = "time_of_saving_articles";
+
     public static final String KEY_CLEAR_GLIDE_CACHE = "clear_glide_cache";
 
 
