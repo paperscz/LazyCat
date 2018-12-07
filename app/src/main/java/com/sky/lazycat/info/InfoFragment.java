aa3a937c3cd7d94273e5fcab26ccfa5baf48a439
Sky <zk827083320@qq.com>
2018-12-07 17:53:13 +0800

    测试。

--- app/src/main/java/com/sky/lazycat/info/InfoFragment.java
+++ app/src/main/java/com/sky/lazycat/info/InfoFragment.java
@@ -4,8 +4,6 @@ package com.sky.lazycat.info;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.widget.NestedScrollView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
