e476052adb65b3271edc4101ccb87fea8e5213e5
Sky <zk827083320@qq.com>
2018-12-14 17:37:06 +0800

    V1.0 LoginActivity。

---
+++ app/src/main/java/com/sky/lazycat/ui/LoginActivity.java
@@ -0,0 +1,22 @@
+package com.sky.lazycat.ui;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.sky.lazycat.R;
+
+/**
+ * 类名称：
+ * 类功能：
+ * 类作者：Administrator
+ * 类日期：2018/12/14 0014.
+ **/
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
